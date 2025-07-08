2ec421ab2ccb2a9a93f532ae30b2744e107c0fd0
Prabkiratsingh868 <prabkirat54@gmail.com>
2025-07-08 21:16:33 +0530

    Add files via upload

---
+++ Chapter 1 C Language/Print_Data_types.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+
+    float b;
+    b = 8.65;
+
+    char c ='p';
+
+    printf("The value of a is %d\n" , a);
+    printf("The value of b is %f\n" , b); 
+    printf("The value of c is %c\n" , c);
+
+    // \n is used for new line
+
+
+    return 0;
+}
