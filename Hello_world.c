3782ce8c075b3e7c308a6ebb998c66fcb32d4998
Prabkiratsingh868 <prabkirat54@gmail.com>
2025-07-08 20:43:01 +0530

    Add files via upload

---
+++ Hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>   //Preprocessor directive
+
+int main(){  //main function
+    printf("Hello World"); //printf function
+    return 0; //return statement
+}
---
+++ comments.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){ //This is single line Comments
+    printf("Hello World");
+    return 0;
+}
+/*
+user = prabkirat singh
+password = CodinginC
+type = english
+
+
+This is a multi line Comments
+*/ 
+// Comments in C doesnot executed it is readable for the users.
