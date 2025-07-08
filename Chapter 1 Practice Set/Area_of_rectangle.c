efa66fd59e7a5d4c076376d2ce68b60d3cc8847c
Prabkirat_s <prabkirat54@gmail.com>
2025-07-08 21:44:54 +0530

    Add files via upload

---
+++ Chapter 1 Practice Set/Area_of_rectangle.c
@@ -0,0 +1,16 @@
+// Write the Program to calculate the area of rectangle using hard coded inputs
+
+// The formula of area of rectangle is length*breath
+
+#include <stdio.h>
+
+int main(){
+    int length = 10;
+    int breath = 10;
+    printf("The area of rectangle is %d" , length*breath);
+
+    
+    
+    return 0;
+}
+
