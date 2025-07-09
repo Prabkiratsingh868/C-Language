f63a605ceb67ba13d7c5fe7cfa3acf1430818fb8
Prabkirat_s <prabkirat54@gmail.com>
2025-07-09 09:39:25 +0530

    Add files via upload

---
+++ Chapter 1 Practice Set/Area_of_cirrcle.c
@@ -0,0 +1,13 @@
+// Calculate the area of circle
+
+// the area of circle is pie r square
+// the value of pie is 3.14
+
+#include <stdio.h>
+
+int main(){
+    int r = 6;
+    printf("The area of circle with its radius is %d is %f" , r , 3.14*r*r);
+
+    return 0;
+}
---
+++ Chapter 1 Practice Set/Volume_of_cylinder.c
@@ -0,0 +1,16 @@
+// Calculate the volume of cylinder given its radius and height
+
+// the volume of cylinder is pie r square h
+// the value of pie is 3.14
+
+// r is radius and h is height
+
+#include <stdio.h>
+
+int main(){
+    int r = 10;
+    int h = 2;
+    printf("The volume of cylinder with radius %d and height %d is %f" , r , h , 3.14*r*r*h);
+
+    return 0;
+}
