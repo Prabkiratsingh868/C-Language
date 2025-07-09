2b0986f72c0bfe05d029dc0ea3b540791cc49201
Prabkirat_s <prabkirat54@gmail.com>
2025-07-09 11:22:14 +0530

    Add files via upload

---
+++ Chapter 2 C Language/All_airthmetic_instructions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a = 100;
+    int b = 20;
+    printf("The addition of a and b is %d\n" , a+b); // addition + 
+
+    printf("The substraction of a and b is %d\n" , a-b); // substraction -
+
+    printf("The multiplication of a and b is %d\n" , a*b); //multiplication *
+
+    printf("The division of a and b is %d\n" , a/b); // division /
+
+    printf("The remainder when a divided by b is %d\n" , a%b); // modulas %
+    // the remainder when 100%20 is 0 because it is divisible by 20
+
+    return 0;
+
+}
---
+++ Chapter 2 C Language/Modulas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a = -100;
+    int b = 3;
+    printf("The value when a is divided by b is %d" , a%b); // The sign of numerator is same as minus
+    // It cannot applied on float
+    // It returns the remainder
+    // % is a module operator
+
+
+    return 0;
+}
