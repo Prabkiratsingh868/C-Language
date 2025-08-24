41eeae21173a14d483cfabb9cbe89a7eeae0a8ee
Prabkirat_s <prabkirat54@gmail.com>
2025-08-24 22:07:27 +0530

    Add files via upload

---
+++ Chapter 11 Practice Set/Problem_3.c
@@ -0,0 +1,17 @@
+// Solve problem 1 using calloc()
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n = 6;
+    int* ptr;
+
+    ptr = (int*) calloc(n, sizeof(int));
+    ptr[0] = 6;
+
+    printf("%d" , ptr[0]);
+    
+
+    return 0;
+}
---
+++ Chapter 11 Practice Set/Problem_6.c
@@ -0,0 +1,37 @@
+// Attempt problem 4 using calloc()
+
+// Problem 4: Create an array dynamically capable of storing 5 integers. Now use realloc so that it can now store 10 integers
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n = 5;
+    int* ptr;
+
+    ptr = (int*)calloc(n , sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d" , &ptr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n" , ptr[i]);
+    }
+    n = 10;
+
+    ptr = (int*) realloc(ptr, 10* sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d" , &ptr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n" , ptr[i]);
+    }
+    
+
+    return 0;
+}
